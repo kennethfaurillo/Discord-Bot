a8e26ed1d1a0fa56dba97ebc5018741297e50ef7
kennethfaurillo <kenneth.edison.faurillo@eee.upd.edu.ph>
2021-09-18 03:57:15 +0800

    kinda working
    Now locally saves songs
    play can now work like resume
    Added random responses
    
    To add next:
    Queueing

--- main.py
+++ main.py
@@ -3,65 +3,70 @@ import re
 import youtube_dl
 import os
 from discord import ClientException
-play_status = 0 # 0 - stopped/no song, 1 - playing, 2 - paused
 from ytsearch import yt_search
+import time
+import random
 from discord.ext import commands
+
+
+play_status = 0 # 0 - stopped/no song, 1 - playing, 2 - paused
+to_reply = False
 
 # client = discord.Client()
 client = commands.Bot(command_prefix='`')
 
 @client.command()
 async def play(ctx, *args):
-    song = os.path.isfile('song.mp3')
+    song_dir = os.path.abspath(os.getcwd()) + "\\songs"
     keyword = ' '.join(args)
-    try:
-        if song:
-            os.remove("song.mp3")
-    except PermissionError:
-        global play_status
-        if play_status == 1:
-            await ctx.send("nag pplay na baga")
-        return
     if not keyword.strip(' '):
+        global play_status
+        if play_status == 2:
+            await resume(ctx)
+            return
         await ctx.send("ha ano ipplay ko?")
         return
-    # try:
-    #     voice_channel = discord.utils.get(ctx.guild.voice_channels, name="General")#name=str(ctx.author.voice.channel))
-    # except AttributeError:
-    #     await ctx.send('bruh join ka muna voice channel lol')
-    #     return
-    voice_channel = discord.utils.get(ctx.guild.voice_channels, name="General")#name=str(ctx.author.voice.channel))
-    try:
+
+    if ctx.author.voice is None:
+        await ctx.send('bruh join ka muna voice channel lol')
+        return
+    voice_channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
         await voice_channel.connect()
-    except ClientException:
-        pass
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    else:
+        await ctx.voice_client.move_to(voice_channel)
+    voice = ctx.voice_client
+
+    start = time.time()
     song_url = yt_search(keyword)
+    print(time.time() - start)
     await ctx.send(song_url)
+
     print(song_url)
     ydl_opts = {
         'format': 'bestaudio/best',
+        'outtmpl': 'songs/%(id)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
-            'preferredquality': '192'
+            'preferredquality': '192',
         }]
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([song_url])
-    for file in os.listdir("./"):
-        if file.endswith(".mp3"):
-            os.rename(file, "song.mp3")
-    print('song start')
+        if os.path.isfile(song_dir+"\\"+song_url[-11:]+'.mp3'):
+            print('local copy found!')
+        else:
+            await ctx.send('wait lang nag ddownload pa')
+            ydl.download([song_url])
+            await ctx.channel.purge(limit=1)
     play_status = 1
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
-    print('song end')
+    # add try except, if except, add to queue
+    voice.play(discord.FFmpegPCMAudio(song_dir+"\\"+song_url[-11:]+'.mp3'), after=print("tapos na po"))
 
 
 @client.command()
 async def leave(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    print(voice)
     if voice:
         await voice.disconnect()
         await ctx.send("kbye")
@@ -72,22 +77,35 @@ async def pause(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_playing():
         voice.pause()
+        global play_status
+        play_status = 2
     else:
         await ctx.send("nothing's playing")
 
 
 @client.command()
 async def resume(ctx):
+    global play_status
+    if play_status == 0:
+        await ctx.send("ano man ireresume mo")
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_paused():
         voice.resume()
+        play_status = 1
     else:
-        await ctx.send("audio is not paused")
+        if not play_status:
+            await ctx.send("no song to play")
+        else:
+            await ctx.send("audio is not paused")
+
 
 @client.command()
 async def stop(ctx):
+    global play_status
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     voice.stop()
+    play_status = 0
+
 
 @client.event
 async def on_message(message):
@@ -95,33 +113,34 @@ async def on_message(message):
     ctx = await client.get_context(message)
     if message.author == client.user:
         return
+    global to_reply
     print('message',message.author, msg)  # debugging purposes
     if msg.startswith('<@!888363419646984222>'):
         if re.search(r'delete', msg):
             await message.channel.purge(limit=2)
             return
         if msg.endswith('<@!888363419646984222>'):
-            reply = "ano "
-            if str(message.author) == "kennethfau#9316":
-                reply += "po boss"
+            reply = random.choice(["ano", "op", "yo", "?", "nukaman", "👋"])
+            if str(message.author) == "kennethfau#9317":
+                reply = "ano po boss"
             else:
-                reply += str(message.author)[:-5]
-            await message.channel.send(reply)
-        msg = msg.split()
-        # PLAY command start
-        if msg[1] in ['p', 'play']:
-            await play(ctx, ' '.join(msg[2:]))
-        # PLAY command end
-    await client.process_commands(message)
-
-
-
+                reply += ' ' + str(message.author)[:-5]
+            to_reply = True
+        else:
+            reply = 'oks'
+        await message.channel.send(reply)
+        return
 
     # Auto replies lol
     ka_man = re.search(r'hi|hello|hai|hoy|pota|hayop|cute|makanos|test', msg)
     if ka_man is not None:
         print(ka_man[0] + " ka man")
         await message.channel.send(ka_man[0] + " ka man")
+        to_reply = False
+    if to_reply:
+        await message.channel.send(random.choice(["ge", "bala ka", "oks", ".", "👍"]))
+        to_reply = False
+    await client.process_commands(message)
 
 
 client.run('ODg4MzYzNDE5NjQ2OTg0MjIy.YURm6A.ajqhThM9Tm05f5ni718vrS9LauA')
