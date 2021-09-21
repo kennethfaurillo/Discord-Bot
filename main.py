cb4e578bdb0e7b0febcc93e799dc3f825c05ac12
kennethfaurillo <kenneth.edison.faurillo@eee.upd.edu.ph>
2021-09-22 00:57:34 +0800

    0.2.0
    Playlist support
    Supported keyword remove, keyword jump, and keyword move
    Added queue clear
    Added queue shuffle
    Added timeout
    Added move command
    Added back youtube_dl (faster extract_info for known urls/playlists)

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import re
 import random
 import asyncio
 
+from youtube_dl import YoutubeDL
 from pytube import YouTube
+from pytube import Playlist
 from discord.ext import commands
 from ytsearch import yt_search
 
-
-play_status = 0  # 0 - stopped/no song, 1 - playing, 2 - paused
 to_reply = False
 
 ffmpeg_opts = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 3', 'options': '-vn'}
@@ -17,8 +17,7 @@ play_list = []
 now_playing = ''
 server = ''
 
-
-client = commands.Bot(command_prefix='`')
+client = commands.Bot(command_prefix="`")
 
 
 def song_done_check(ctx):
@@ -28,130 +27,187 @@ def song_done_check(ctx):
         fut.result()
     except:
         print('nag error')
-        leave(ctx)
 
 
+# temp command
 async def song_done(ctx):
     print('tapos na po')
-    if play_list:
-        await play(ctx, autoplay=True)
-    else:
-        print("playlist done")
-        global play_status
-        play_status = 0
+    await play(ctx, autoplay=True)
 
 
 @client.command(name="Join", brief="    -    `join", aliases=['join'])
 async def join(ctx):
     if ctx.author.voice is None:
         await ctx.send('bruh join ka muna voice channel lol')
-        return
+        return None
 
     voice_channel = ctx.author.voice.channel
     if ctx.voice_client is None:
         await voice_channel.connect()
     else:
         await ctx.voice_client.move_to(voice_channel)
+    return ctx.voice_client
+
+
+async def play_helper(ctx, song_url):
     voice = ctx.voice_client
-    return voice
+    try:
+        voice.play(await discord.FFmpegOpusAudio.from_probe(song_url, **ffmpeg_opts),
+                   after=lambda _: song_done_check(ctx))
+        embed = discord.Embed(title=f'Now Playing:    {now_playing}')
+        await ctx.send(embed=embed)
+    except discord.errors.ClientException:
+        await ctx.send('wait lang')
+        pass
 
 
 @client.command(name="Play", brief="    -    `p | `play", aliases=["play", 'p', 'P'])
 async def play(ctx, *args, autoplay=False):
-    if ctx.author.voice is None:
-        await ctx.send('bruh join ka muna voice channel lol')
-        return
-
-    global play_status
     global now_playing
-    if args:
-        if not play_list and not now_playing:
-            check = await queue(ctx, *args, no_message=True)
-        else:
-            check = await queue(ctx, *args, no_message=False)
-        if check == -1 or play_status == 1:
-            return
-    else:
-        if not play_list and not now_playing:
-            await ctx.send("ano man ipplay ko")
-            return
-        if play_status and not autoplay:
-            if play_status == 2:
-                print('resume via play')
-                await resume(ctx)
-            return
-
     voice = await join(ctx)
+    if not voice:
+        return
+    if not autoplay:
+        if args:  # Song keyword/link given
+            if not play_list and not now_playing:
+                queue_check = await queue(ctx, *args, no_message=True, from_play=True)
+            else:
+                queue_check = await queue(ctx, *args, no_message=False, from_play=True)
+            if not queue_check or voice.is_playing():  # possible bug
+                return
+        else:  # just `play
+            if not play_list and not now_playing:
+                await ctx.send("ano man ipplay ko")
+                return
+            if not autoplay:  # possible bug
+                if voice.is_paused():
+                    print('resume via play')
+                    await resume(ctx)
+                    return
+                elif voice.is_playing():
+                    await ctx.send("nagpplay na baga")
+                    return
 
     # Allow ffmpeg to reconnect
-
-    play_status = 1
-
     try:
         now_playing, song_url = play_list.pop(0)
     except IndexError:
         now_playing = ''
-        play_status = 0
         print('ubos na playlist')
+        await timeout(ctx, 30)
         return
+    await play_helper(ctx, song_url)
 
-    try:
-        embed = discord.Embed(title=f'Now Playing:    {now_playing}')
+
+async def queue_helper(ctx, url_list, no_message, pl_name=None, from_play=False):
+    global now_playing
+    is_playlist = bool(pl_name)
+    if is_playlist:
+        embed = discord.Embed(title=f"Added {len(url_list)} songs to queue from {pl_name} playlist")
         await ctx.send(embed=embed)
-        voice.play(await discord.FFmpegOpusAudio.from_probe(song_url, **ffmpeg_opts), after=lambda _: song_done_check(ctx))
-    except discord.errors.ClientException:
-        pass
+
+    for idx, yt in enumerate(url_list):
+        song_url = YoutubeDL().extract_info(yt.watch_url, download=False)['formats'][0]['url']
+        if pl_name and not idx:
+            if ctx.voice_client and from_play:
+                now_playing = yt.title
+                await play_helper(ctx, song_url)
+                continue
+        song_title = yt.title
+        if not is_playlist:
+            await ctx.send(yt.watch_url, delete_after=7.5)
+        if not is_playlist and not no_message:
+            embed = discord.Embed(title=f"Added:    {song_title} to queue")
+            await ctx.send(embed=embed)
+        play_list.append([song_title, song_url])
 
 
 @client.command(name='Queue', brief="    -    `q | `queue ", aliases=['queue', 'q'])
-async def queue(ctx, *args, no_message=False):
-    """
-    Previews song queue (only yt links for now)
-    """
+async def queue(ctx, *args, no_message=False, from_play=False):
     global server
     server = ctx.guild
     keywords = ' '.join(args)
-    if not keywords.strip(' '):
+    voice = ctx.voice_client
+    if not keywords.strip():  # Show the queue
         songs = ''
         for idx, x in enumerate(play_list):
-            songs += (str(idx+1) + ':    ' + x[0] + '\n')
+            songs += (str(idx + 1) + ':    ' + x[0] + '\n')
         songs = songs or "uda na kanta"
-        if play_status:
+        if voice and (voice.is_playing() or voice.is_paused()):
             title = f"Now Playing:    {now_playing}"
             embed = discord.Embed(title=title, description=songs)
         else:
             embed = discord.Embed(description=songs)
         await ctx.send(embed=embed)
         return
+    else:  # Add to queue
+        pl_name = None
+        if 'youtube.com/playlist?' in keywords:     # Playlist
+            keywords_check = keywords.split()
+            pl = Playlist(keywords_check[0])
+            num_songs = len(pl)
+            if len(keywords_check) > 1:             # number of songs to add given
+                num_songs = int(keywords_check[-1])
+            to_queue = list(pl.videos)[:num_songs]
+            pl_name = pl.title
+        else:
+            video_url = await yt_search(keywords)   # Single (faster than ytdl)
+            if not video_url:
+                await ctx.send("sowi di ko mahanap 😢")
+                return False
+            to_queue = [YouTube(video_url)]
+        await queue_helper(ctx, to_queue, no_message, pl_name, from_play)
+        return True
 
+
+def keyword_to_index(keyword):
+    lower_case_titles = [x[0].lower() for x in play_list]
+    keyword = keyword.lower()
+    for idx, lower_case_title in enumerate(lower_case_titles):
+        if keyword in lower_case_title:
+            return idx + 1
+    else:
+        return keyword
+
+
+@client.command(name='Move', brief="    -    `mv | `move", aliases=['move', 'mv'])
+async def move(ctx, source_index, dest_index):
+    if not source_index.isdigit():  # keyword is given
+        source_index = keyword_to_index(source_index)
+        if not type(source_index) == int:
+            await ctx.send(f"uda man {source_index}")
+            return
+    if not dest_index.isdigit():  # keyword is given
+        dest_index = keyword_to_index(dest_index)
+        if not type(dest_index) == int:
+            await ctx.send(f"uda man {dest_index}")
+            return
+    source_index, dest_index = int(source_index), int(dest_index)
+    if source_index == dest_index or (max(int(source_index), int(dest_index)) > len(play_list)):
+        await ctx.send("???")
+        return
     try:
-        video_url = await yt_search(keywords)
-    except AttributeError:
-        await ctx.send("sowi di ko mahanap 😢")
-        return -1
-
-    yt = YouTube(video_url)
-    song_url = yt.streaming_data.get('hlsManifestUrl') or yt.streams.get_audio_only().url
-    song_title = yt.title
-    print(video_url, '\n', song_title)
-    print(song_url)
-
-    await ctx.send(video_url, delete_after=7.5)
-
-    embed = discord.Embed(title=f"Added:    {song_title} to queue")
-    if not no_message:
+        song_to_move = play_list.pop(int(source_index)-1)
+        play_list.insert(int(dest_index)-1, song_to_move)
+        embed = discord.Embed(title=f"Moved {song_to_move[0]} to {dest_index}")
         await ctx.send(embed=embed)
-    play_list.append([song_title, song_url])
-    return song_url
+    except IndexError:
+        await ctx.send("bug")
+
+
+async def timeout(ctx, secs=30):
+    await asyncio.sleep(secs)
+    voice = ctx.voice_client
+    if not voice.is_paused() and not voice.is_playing():
+        await leave(ctx)
 
 
 @client.command(name='Leave', brief="    -    `leave | `disconnect", aliases=['leave', 'disconnect'])
 async def leave(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice:
-        global play_list, play_status, server, now_playing
+        global play_list, server, now_playing
         play_list = []
-        play_status = 0
         server = ''
         now_playing = ''
         await voice.disconnect()
@@ -163,27 +219,22 @@ async def pause(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if not voice:
         await ctx.send("ano man ipapause ko")
-
     if voice.is_paused():
         await ctx.send("naka pause na baga")
         return
     elif voice.is_playing():
         voice.pause()
-        global play_status
-        play_status = 2
         embed = discord.Embed(title=f"Paused:    {now_playing}")
         await ctx.send(embed=embed)
 
 
 @client.command(name='Resume', brief="    -    `resume | `res ", aliases=['resume', 'res'])
 async def resume(ctx):
-    global play_status
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if not voice:
         await ctx.send("ano man ireresume ko")
     if voice.is_paused():
         voice.resume()
-        play_status = 1
         await nowplaying(ctx)
     elif voice.is_playing():
         await ctx.send("nag pplay na baga")
@@ -193,9 +244,6 @@ async def resume(ctx):
 
 @client.command(name='Skip', brief="    -    `skip | `s", aliases=['skip', 's'])
 async def skip(ctx, *args, message=True):
-    if args:
-        await jump(ctx, *args)
-        return
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     global now_playing
     if voice and (voice.is_playing() or voice.is_paused()):
@@ -203,8 +251,7 @@ async def skip(ctx, *args, message=True):
             embed = discord.Embed(title=f"Skipping {now_playing}")
             await ctx.send(embed=embed)
         voice.stop()
-        now_playing = ''
-        # play_status = 2 # changed
+        # await play(ctx, autoplay=True)
     else:
         await ctx.send("ano isskip ko")
 
@@ -216,17 +263,21 @@ async def jump(ctx, song_index=''):
         await ctx.send("gib number plx")
         return
     try:
+        if not song_index.isdigit():                            # keyword is given
+            lower_case_titles = [x[0].lower() for x in play_list]
+            song_index = song_index.lower()
+            for idx, lower_case_title in enumerate(lower_case_titles):
+                if song_index in lower_case_title:
+                    song_index = idx + 1
+                    break
         voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-
-        # song_removed = play_list.pop(int(song_index)-1)[0]
-        play_list = play_list[int(song_index)-1:]
-        embed = discord.Embed(title="Skipping to "+play_list[0][0])
+        play_list = play_list[int(song_index) - 1:]
+        embed = discord.Embed(title="Skipping to " + play_list[0][0])
         await ctx.send(embed=embed)
         if voice:
-            await skip(ctx, message=False)
+            await skip(ctx, False)
     except IndexError:
-        await ctx.send("uda man "+song_index)
-    # if jump 1, just send to skip (same behavior maybe no message, maybe include it)
+        await ctx.send("uda man " + song_index)
 
 
 @client.command(name="Now Playing", brief="    -    `np | `NP  ", aliases=['np', 'NP'])
@@ -240,11 +291,32 @@ async def remove(ctx, song_index=''):
     if song_index == '':
         await ctx.send("???")
     try:
-        song_removed = play_list.pop(int(song_index)-1)[0]
-        embed = discord.Embed(title="Removed "+song_removed)
+        if not song_index.isdigit():  # keyword is given
+            if song_index.lower() in now_playing.lower():
+                await ctx.send("cannot remove nagpplay, baka skip?")
+                return
+            song_index = keyword_to_index(song_index)
+        song_removed = play_list.pop(int(song_index) - 1)[0]
+        embed = discord.Embed(title="Removed " + song_removed)
         await ctx.send(embed=embed)
-    except IndexError:
-        await ctx.send("uda man "+song_index)
+    except (ValueError, IndexError):
+        await ctx.send("uda man " + song_index)
+
+
+@client.command(name="Clear", brief="    -    `clear", aliases=['clear'])
+async def clear(ctx):
+    play_list.clear()
+    embed = discord.Embed(title="Cleared queue")
+    await ctx.send(embed=embed)
+
+
+@client.command(name="Shuffle", brief="    -    `shuffle", aliases=['shuffle'])
+async def shuffle(ctx):
+    if not play_list:
+        await ctx.send("uda man play list")
+    random.shuffle(play_list)
+    embed = discord.Embed(title="Queue shuffled")
+    await ctx.send(embed=embed)
 
 
 @client.command(name="Commands", brief="    -    Shows this message", aliases=['commands'])
@@ -266,13 +338,13 @@ async def auto_reply(ctx, msg):
 
 @client.event
 async def on_ready():
-    print(str(client.user)+" Live")
+    print(str(client.user) + " Live")
 
 
 @client.event
 async def on_message(message):
     # Server check
-    print('chat:    '+str(message.author)[:-5]+': '+message.content)  # debugging purposes
+    print('chat:    ' + str(message.author)[:-5] + ': ' + message.content)  # debugging purposes
 
     if message.content.startswith('`'):
         if server and message.guild != server:
@@ -283,7 +355,6 @@ async def on_message(message):
 
     msg = message.content.lower()
     if message.author == client.user:
-        print("chat:    replied: "+msg)
         return
 
     global to_reply
@@ -296,12 +367,13 @@ async def on_message(message):
             if str(message.author) == "kennethfau#9317":
                 reply = "ano po boss"
             else:
-                reply += ' ' + "<@"+str(message.author.id)+">"
+                reply += ' ' + "<@" + str(message.author.id) + ">"
             to_reply = True
         else:
             reply = 'oks'
         await message.channel.send(reply)
         return
     await auto_reply(await client.get_context(message), msg)
+
 
 client.run(os.getenv("TOKEN"))
--- ytsearch.py
+++ ytsearch.py
@@ -1,7 +1,5 @@
-import urllib.request
 import re
 import aiohttp
-import asyncio
 
 
 async def yt_search(keyword):
@@ -16,6 +14,8 @@ async def yt_search(keyword):
     async with aiohttp.ClientSession() as session:
         async with session.get("https://www.youtube.com/results?search_query="+keyword) as resp:
             html = await resp.text()
-    video_id = re.search(r"watch\?v=(\S{11})", html).group()[-11:]
-    url_first_result = "https://www.youtube.com/watch?v="+video_id
+    video_id = re.search(r"watch\?v=(\S{11})", html)
+    if not video_id:
+        return None
+    url_first_result = "https://www.youtube.com/watch?v="+video_id.group()[-11:]
     return url_first_result  
